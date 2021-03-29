1b94d492d8842bd85b4161568d5ab59d4f82e32f
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2021-03-29 09:58:22 -0300

    feat: added swagger docs

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
+import ui from 'swagger-ui-express'
+import uiFile from './swagger.json'
+
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", ui.serve, ui.setup(uiFile))
 app.use("/users", usersRoutes);
 
 export { app };
